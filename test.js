a4cfd0faf10911b298fd2fd05279ee4c2051a3a8
Michael Conrads <michael.conrads@quartett-mobile.de>
2017-01-07 16:14:35 +0100

    test websocket server

---
+++ test.js
@@ -0,0 +1,49 @@
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer({ port: 8080 });
+
+var connectedChannels = []
+wss.on('connection', function connection(ws) {
+    console.log("connection established")
+    connectedChannels.push(ws)
+
+    console.log(connectedChannels.length)
+
+    ws.on('close', function close() {
+	console.log('disconnected');
+	connectedChannels.splice(connectedChannels.indexOf(ws), 1)
+    });
+    
+    ws.on('message', function incoming(message) {
+	message = JSON.parse(message)
+	
+	console.log("")
+	console.log(message)
+	console.log("")
+
+	var resp =  message.data.clientid + ": " + message.data.textresponse
+	for (var i = 0; i < connectedChannels.length; i++) {
+	    send(connectedChannels[i], resp)
+	}
+
+    });
+    var id = Math.round((Math.random() * 1000))
+    send(ws, id, "idmsg")
+});
+
+
+
+function send(ws, message, type="msg") {
+    var msg = {'type':type, "content": message}
+    var m = JSON.stringify(msg)
+//    console.log(m)
+    ws.send(m);
+}
+
+
+
+
+
+
+
+
+
